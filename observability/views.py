e5c6a0ada5ad11c6425505e44c5c4aff5e560630
rubayetkabirzisan <rubayetkabirz@gmail.com>
2026-06-13 09:57:43 +0600

    added new file in observability

---
+++ observability/views.py
@@ -0,0 +1,22 @@
+﻿from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from django.utils import timezone
+import django
+from ShelfIQ.middleware import get_metrics
+class MetricsView(APIView):
+    permission_classes = [AllowAny]
+    def get(self, request):
+        from visits.models import Visit
+        from fraud.models import FraudLog
+        from analysis.models import ShelfAnalysis
+        metrics = get_metrics()
+        metrics['db'] = {
+            'total_visits':     Visit.objects.count(),
+            'total_analyses':   ShelfAnalysis.objects.count(),
+            'total_fraud_logs': FraudLog.objects.count(),
+            'fraud_confirmed':  FraudLog.objects.filter(is_fraud=True).count(),
+        }
+        metrics['django_version'] = django.get_version()
+        metrics['server_time']    = timezone.now().isoformat()
+        return Response(metrics)
